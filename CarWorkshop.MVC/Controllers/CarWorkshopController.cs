53217d33a6b27f16e36617ab513b31f63074bb55
jacobofernandez420 <j.kozlowski420@interia.pl>
2023-07-29 12:12:20 +0200

    Walidacja modelu CarWorkshop

--- CarWorkshop.MVC/Controllers/CarWorkshopController.cs
+++ CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -20,6 +20,10 @@ namespace CarWorkshop.MVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CarWorkshopDto carWorkshop)
         {
+            if (ModelState.IsValid)
+            {
+                return View(carWorkshop);
+            }
             await _carWorkshopService.Create(carWorkshop);
             return RedirectToAction(nameof(Create));//TODO: refactor
         }
