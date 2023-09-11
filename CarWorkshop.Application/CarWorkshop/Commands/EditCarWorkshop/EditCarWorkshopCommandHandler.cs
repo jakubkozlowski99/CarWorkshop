a8151efb187764eaeac478142e8370df4a5fa143
jacobofernandez420 <j.kozlowski420@interia.pl>
2023-09-11 19:00:28 +0200

    Rola moderatora

--- CarWorkshop.Application/CarWorkshop/Commands/EditCarWorkshop/EditCarWorkshopCommandHandler.cs
+++ CarWorkshop.Application/CarWorkshop/Commands/EditCarWorkshop/EditCarWorkshopCommandHandler.cs
@@ -24,7 +24,7 @@ namespace CarWorkshop.Application.CarWorkshop.Commands.EditCarWorkshop
             var carWorkshop = await _repository.GetByEncodedName(request.EncodedName!);
 
             var user = _userContext.GetCurrentUser();
-            var isEditable = user != null && carWorkshop.CreatedById == user.Id;
+            var isEditable = user != null && (carWorkshop.CreatedById == user.Id || user.IsInRole("Moderator"));
 
             if (!isEditable)
             {
