c2347758d50d4d0dc0ca79b04f01a1f2cd8add7a
jacobofernandez420 <j.kozlowski420@interia.pl>
2023-09-18 04:06:12 +0200

    Toastr notyfikacje

---
+++ CarWorkshop.MVC/Extensions/ControllerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CarWorkshop.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarWorkshop.MVC.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static void SetNotification(this Controller controller, string type, string message)
+        {
+            var notification = new Notification(type, message);
+            controller.TempData["Notification"] = JsonConvert.SerializeObject(notification);
+        }
+    }
+}
