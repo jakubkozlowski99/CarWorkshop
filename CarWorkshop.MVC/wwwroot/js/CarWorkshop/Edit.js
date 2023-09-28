d5c71bccb6fa610ef6ae722a15c219de61a02c55
jacobofernandez420 <j.kozlowski420@interia.pl>
2023-09-28 22:29:53 +0200

    Reużywalny skrypt renderowania usług

--- CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
+++ CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
@@ -1,40 +1,5 @@
 ﻿$(document).ready(function () {
 
-    const RenderCarWorkshopServices = (services, container) => {
-        container.empty();
-
-        for (const service of services) {
-            container.append(
-              `<div class="card border-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">${service.cost}</div>
-                <div class="card-body">
-                    <h5 class="card-title">${service.description}</h5>
-                </div>
-              </div>`
-            )
-        }
-    }
-
-    const LoadCarWorkshopServices = () => {
-        const container = $("#services")
-        const carWorkshopEncodedName = container.data("encodedName");
-
-        $.ajax({
-            url: `/CarWorkshop/${carWorkshopEncodedName}/CarWorkshopService`,
-            type: 'get',
-            success: function (data) {
-                if (!data.length) {
-                    container.html("There are no services for this car workshop")
-                } else {
-                    RenderCarWorkshopServices(data, container)
-                }
-            },
-            error: function () {
-                toastr["error"]("Something went wrong")
-            }
-        })
-    }
-
     LoadCarWorkshopServices()
 
     $("#createCarWorkshopServiceModal form").submit(function (event) {
@@ -46,6 +11,7 @@
             data: $(this).serialize(),
             success: function (data) {
                 toastr["success"]("Created carworkshop service")
+                LoadCarWorkshopServices()
             },
             error: function () {
                 toastr["error"]("Something went wrong")
